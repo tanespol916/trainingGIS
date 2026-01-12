a0d384b0277518ce51477620d87078d68febf9d5
unknown <tanespol.pra@gmail.com>
2026-01-12 13:52:09 +0700

    complete Day3

--- src/app/pages/demo-gis-page/demo-gis-page.ts
+++ src/app/pages/demo-gis-page/demo-gis-page.ts
@@ -6,6 +6,11 @@ import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js";
 import Graphic from "@arcgis/core/Graphic.js";
 import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
+import PictureMarkerSymbol from "@arcgis/core/symbols/PictureMarkerSymbol.js";
+import VectorTileLayer from "@arcgis/core/layers/VectorTileLayer.js";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer.js";
+import CSVLayer from "@arcgis/core/layers/CSVLayer.js";
+import HeatmapRenderer from "@arcgis/core/renderers/HeatmapRenderer.js";
 
 @Component({
   selector: 'app-demo-gis-page',
@@ -14,10 +19,31 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './demo-gis-page.css',
 })
 export class DemoGisPage implements OnInit, OnDestroy, AfterViewInit {
+
+
   @ViewChild('mapViewNode', { static: false }) private mapViewEl!: ElementRef;
   map: Map | null = null;
   mapView: MapView | null = null;
+  csvLayer: CSVLayer | null = null;
+  defaultCsvRenderer: any = null;
   demoGraphicsLayer = new GraphicsLayer();
+  demoVectorTileLayer = new VectorTileLayer({
+    url: "https://tiles.arcgis.com/tiles/jSaRWj2TDlcN1zOC/arcgis/rest/services/Thailand_Transportation/VectorTileServer"
+  });
+  demoFeatureLayer = new FeatureLayer({
+    url: "https://services-ap1.arcgis.com/iA7fZQOnjY9D67Zx/ArcGIS/rest/services/OSM_AS_POIs/FeatureServer/0",
+    outFields: ["*"],
+    popupTemplate: {
+      title: "{name}",
+      content:
+        "<b>Type:</b> {amenity} <br><b>Place:</b> {place} <br>",
+    }
+  });
+  provinceFeatureLayer = new FeatureLayer({
+    url: "https://services1.arcgis.com/jSaRWj2TDlcN1zOC/ArcGIS/rest/services/Thailand_Province_Boundaries_view/FeatureServer/1",
+    outFields: ["*"]
+  });
+
 
   ngOnInit(): void {
   }
@@ -36,10 +62,10 @@ export class DemoGisPage implements OnInit, OnDestroy, AfterViewInit {
     this.mapView = new MapView({
       container,
       map: this.map,
-      center: [100.5433989, 13.7029924], // longitude, latitude
+      center: [100.61238932664604, 14.073619468605445], // longitude, latitude
       zoom: 20,
-    });
 
+    });
     this.map.add(this.demoGraphicsLayer);
 
     this.mapView.when(() => { });
@@ -47,12 +73,167 @@ export class DemoGisPage implements OnInit, OnDestroy, AfterViewInit {
     return this.mapView.when();
   }
 
+  hideCsvLayer() {
+  if (!this.csvLayer) return;
+  this.csvLayer.visible = false;
+}
+
+  resetAnalysis() {
+    if (!this.csvLayer || !this.defaultCsvRenderer) return;
+    this.csvLayer.renderer = this.defaultCsvRenderer;
+  }
+
+  analysis() {
+    if (!this.csvLayer) {
+      return;
+    }
+    if (!this.defaultCsvRenderer) {
+      this.defaultCsvRenderer = this.csvLayer.renderer;
+    }
+
+    const heatmapRenderer = new HeatmapRenderer({
+      minDensity: 0,
+      maxDensity: 1,
+      colorStops: [
+        { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+        { color: "#472b77", ratio: 0.083 },
+        { color: "#4e2d87", ratio: 0.166 },
+        { color: "#563098", ratio: 0.249 },
+        { color: "#5d32a8", ratio: 0.332 },
+        { color: "#6735be", ratio: 0.415 },
+        { color: "#7139d4", ratio: 0.498 },
+        { color: "#7b3ce9", ratio: 0.581 },
+        { color: "#853fff", ratio: 0.664 },
+        { color: "#a46fbf", ratio: 0.747 },
+        { color: "#c29f80", ratio: 0.83 },
+        { color: "#e0cf40", ratio: 0.913 },
+        { color: "#ffff00", ratio: 1 },
+      ]
+    });
+
+    this.csvLayer.renderer = heatmapRenderer;
+  }
+
+
+  addCSV() {
+    if (!this.map || !this.mapView) return;
+
+
+    this.csvLayer = new CSVLayer({
+      url: "https://raw.githubusercontent.com/jeffprosise/Machine-Learning/refs/heads/master/Data/taxi-fares.csv",
+
+      latitudeField: "pickup_latitude",
+      longitudeField: "pickup_longitude",
+
+      popupTemplate: {
+        title: "Taxi ride",
+        content: `
+      <b>Fare:</b> {fare_amount} USD<br/>
+      <b>Pickup Latitude:</b> {pickup_latitude}<br/>
+      <b>Pickup Longitude:</b> {pickup_longitude}
+    `
+      }
+    });
+  
+    this.map.add(this.csvLayer);
+
+    this.mapView.goTo({
+      center: [-74.006, 40.7128],
+      zoom: 11
+    });
+  }
+
+
+  zoomFeatureLayer() {
+    if (!this.map) return;
+    this.provinceFeatureLayer.queryFeatures({
+      where: "NAME1='กรุงเทพมหานคร'",  // SQL Statement
+      returnGeometry: true,
+      outFields: ["*"],
+      num: 1
+    }).then(result => {
+      console.log('result', result)
+      if (result.features.length > 0) {
+        const feature = result.features[0];
+        this.demoGraphicsLayer.removeAll();
+
+        // 🔹 สร้าง symbol สำหรับขอบเขตจังหวัด
+        const boundarySymbol: any = {
+          type: "simple-fill",
+          color: [0, 0, 0, 0], // โปร่งใส
+          outline: {
+            color: [0, 150, 255], // ฟ้า
+            width: 3
+          }
+        };
+
+        // 🔹 สร้าง graphic
+        const boundaryGraphic = new Graphic({
+          geometry: feature.geometry,
+          symbol: boundarySymbol
+        });
+
+        // 🔹 วาดขอบเขตจังหวัดลง graphic layer
+        this.demoGraphicsLayer.add(boundaryGraphic);
+
+        // 🔹 zoom ไปที่ polygon
+        this.mapView?.goTo(
+          {
+            target: feature.geometry,
+            padding: 40
+          },
+          {
+            duration: 1200,
+            easing: "ease-in-out"
+          }
+        );
+      }
+    });
+  }
+
+  addFeatureLayer() {
+    if (!this.map) return;
+
+    if (!this.map.layers.includes(this.demoFeatureLayer)) {
+      this.map.add(this.demoFeatureLayer);
+    } else {
+      this.demoFeatureLayer.visible = true;
+    }
+  }
+
+  hideFeatureLayer() {
+    if (!this.map) return;
+
+    if (this.map.layers.includes(this.demoFeatureLayer)) {
+      this.demoFeatureLayer.visible = false;
+    }
+  }
+
+  addVectorTile() {
+    if (!this.map) return;
+
+    if (!this.map.layers.includes(this.demoVectorTileLayer)) {
+      this.map.add(this.demoVectorTileLayer);
+    } else {
+      this.demoVectorTileLayer.visible = true;
+    }
+  }
+
+  hideVectorTile() {
+    if (!this.map) return;
+
+    if (this.map.layers.includes(this.demoVectorTileLayer)) {
+      this.demoVectorTileLayer.visible = false;
+    }
+  }
+
   addGraphics() {
     // zoom to specific location
     this.mapView?.goTo({
-      center: [-118.805, 34.020],
-      zoom: 13
+      center: [100.61238932664604, 14.073619468605445],
+      zoom: 20
     })
+
     // clear all old graphics
     this.demoGraphicsLayer.removeAll();
 
@@ -123,10 +304,49 @@ export class DemoGisPage implements OnInit, OnDestroy, AfterViewInit {
       popupTemplate: popupTemplate,
     });
     this.demoGraphicsLayer.add(polygonGraphic);
+
+    const IconPoint: any = {
+      //Create a point
+      type: "point",
+      longitude: 100.61238932664604,
+      latitude: 14.073619468605445,
+    };
+    const IconSymbol = new PictureMarkerSymbol({
+      url: "images/photos/school.png",
+      width: "64px",
+      height: "64px"
+    });
+
+    const IconGraphic = new Graphic({
+      geometry: IconPoint,
+      symbol: IconSymbol,
+      attributes: {
+        name: "มหาวิทยาลัยธรรมศาสตร์ ศูนย์รังสิต",
+        image: "https://lh3.googleusercontent.com/gps-cs-s/AG0ilSwd49k6GoOfrdkidJoQedxNjkJKkVLXQJCQyQ187eyDibb2IFQw49Y5gCQVYvVVtACzIykU1rSQZbzWuIZwCT4KPE-Q-gDlBcu8qNlvNUlsOeqrXu3IJ3p6vi4_v6qnsl-_2F-8=w408-h350-k-no",
+        address: "99 หมู่ที่ 18 ถ. พหลโยธิน ต.คลองหนึ่ง อ.คลองหลวง ปทุมธานี 12120"
+      },
+      popupTemplate: {
+        title: "{name}",
+        content: `
+      <div class="w-64">
+        <img 
+          src="{image}" 
+          class="w-full h-40 object-cover rounded-lg mb-3"
+        />
+
+        <p class="text-sm text-gray-700">
+          {address}
+        </p>
+    `
+      }
+    });
+
+    this.demoGraphicsLayer.add(IconGraphic);
+
   }
 
   clearGraphicLayer() {
-	  this.demoGraphicsLayer.removeAll();
+    this.demoGraphicsLayer.removeAll();
   }
 
   ngOnDestroy(): void {
@@ -134,4 +354,5 @@ export class DemoGisPage implements OnInit, OnDestroy, AfterViewInit {
       this.mapView.destroy();
     }
   }
+
 }
